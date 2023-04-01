0e2280c659e04d8783e755baacdacc5581d4f729
Nur Afifah Abdullah <a.pipah021@gmail.com>
2023-04-01 15:16:26 +0800

    alert text change

--- function.js
+++ function.js
@@ -4,10 +4,10 @@ const answer = Math.floor(Math.random() * 10 + 1);
  document.getElementById("submitButton").onclick = function(){
 
     let guess = document.getElementById("guessField").value;
-    guesses+=1;
+    guesses+=1; //this being create start so it will show how many tries the user guess and it will start from one tries. 
 
     if (guess == answer) {
-        alert(`${answer} is the number, it took you ${guesses} guesses!`);
+        alert(`You've got it!! ${answer} is the  correct number, it took you ${guesses} tries to guess it!`);
         window.location.reload(); //to reload page and restart the game, when u guess right
     }
     else if(guess < answer){
